7aaf8cf1abfb743e7cc3870c7f381c95e7123dd9
Nikita <nike.volobuev@gmail.com>
2023-07-19 09:35:28 +0200

    First commit

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
 import express from 'express'
 import mysql from 'mysql'
 import cors from 'cors'
+import 'dotenv/config'
 
 const app = express()
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Vfktymrfz44@',
-    database: 'test'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 })
 
 app.use(express.json())
